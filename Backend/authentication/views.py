1cdfed0fe2470acaed0b6bb8b893b929c2b89674
Lady-Lune <180202109+Lady-Lune@users.noreply.github.com>
2025-08-31 15:43:28 +0530

    made user model and api endpoint to create, update and delete users

--- Backend/authentication/views.py
+++ Backend/authentication/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from .models import User
+from rest_framework import  viewsets
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from .serializers import UserSerializer
 
 # Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny] #change to isAuthenticated later
+    
+    def get_permissions(self):
+        """Allow anyone to register, but require authentication for other actions"""
+        if self.action == 'create':
+            return [AllowAny()]
+        return [AllowAny()] #change to isAuthenticated later
