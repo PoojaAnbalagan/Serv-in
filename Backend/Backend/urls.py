1d6b2a4e918a1d0809e8a9775e4933718170be32
errormakesclever <nithiyathursan123@gmail.com>
2025-09-02 23:26:50 +0530

    updation on settings.py and urls.py

--- Backend/Backend/urls.py
+++ Backend/Backend/urls.py
@@ -15,9 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+# urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/services/", include("services.urls")),
+    path("api/users/", include("users.urls")),
+    path("api/bookings/", include("bookings.urls")),
+    path("api/reviews/", include("reviews.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
