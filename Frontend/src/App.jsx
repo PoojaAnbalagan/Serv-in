3bbb89bbb59a9d30738760888999037680055262
PoojaAnbalagan <22cds0412@ms.sab.ac.lk>
2025-09-06 08:37:34 +0530

    testing tailwind css

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,23 +1,14 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import RegisterForm from "./Components/RegisterForm/RegisterForm";
-import LoginForm from "./Components/LoginForm/LoginForm";
-import Dashboard from "./Components/Dashboard/Dashboard";
+import "./App.css"
 
 function App() {
-  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
 
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Navigate to="/register" />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/login" element={<LoginForm />} />
-        <Route
-          path="/dashboard"
-          element={isLoggedIn ? <Dashboard /> : <Navigate to="/login" />}
-        />
-      </Routes>
-    </Router>
+    <div className="text-3xl text-amber-800 bg-amber-200 font-bold underline mt-2.5 p-3.5 text-center border-amber-950 border-4 rounded-4xl font-serif">
+      Hello world
+    </div>
+    
   );
 }
 
